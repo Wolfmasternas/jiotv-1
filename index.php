1a48ab6160e5bb5fd534c17f457f84737aba1f6a
AMJID DADER <amjiddader@gmail.com>
2019-06-25 16:25:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,83 +1,12 @@
-<html> <title> STREAMSAW LIVE STREAMING </title>
-<h2> SPORTS </h2>
+<html> <title> JIO TV SCRIPT  </title>
 
+Need help let me know..  <br>
+  To watch a channel on localhost you can watch channels directly in HD and SD <br>
+  
 
+<a href="http://localhost/hd.php?c=AXN_HD"target="_blank">AXN HD high<a> | 
+<a href="http://localhost/sd.php?c=AXN_HD"target="_blank">AXN HD low<a> | <br>
 
+You can change localhost to IP / Domain of your server if you want to watch on VPS <br> 
+Regards 
 
-
-
-
-<h2> ENGLISH MOVIES </h2>
-<a href="http://localhost/geo/play.php?AXN_HD&q=1200"target="_blank">AXN HD<a> | 
-<a href="http://localhost/geo/play.php?WB&q=1200"target="_blank">WB MOVIES<a> | 
-<a href="http://localhost/geo/play.php?HBO_HD&q=1200"target="_blank">HBO MOVIES<a> | 
-<a href="http://localhost/geo/play.php?Sony_Pix_HD&q=1200"target="_blank">SONY PIX <a> | 
-<a href="http://localhost/geo/play.php?MNPLUS&q=1200"target="_blank">MN+<a> | 
-<a href="http://localhost/geo/play.php?Romedy_Now_HD&q=1200"target="_blank">ROMEDY NOW <a> | 
-
-
-
-
-
-<h2> HINDI MOVIES </h2>
-<a href="http://localhost/geo/play.php?Zee_Cinema_HD&q=1200"target="_blank">ZEE CENIMA HD<a> | 
-<a href="http://localhost/geo/play.php?Zee_Classic&q=1200"target="_blank">ZEE CLASSIC<a> | 
-
-
-<h2> HINDI GENERAL </h2>
-<a href="http://localhost/geo/play.php?DD_National&q=1200"target="_blank">DD NATIONAL<a> | 
-<a href="http://localhost/geo/play.php?And_TV_HD&q=1200"target="_blank">&TV HD<a> | 
-<a href="http://localhost/geo/play.php?Sony_HD&q=1200"target="_blank">SONY TV<a> | 
-<a href="http://localhost/geo/play.php?Zee_TV_HD&q=1200"target="_blank">ZEE TV HD<a> | 
-<a href="http://localhost/geo/play.php?Colors_HD&q=1200"target="_blank">COLORS HD<a> | 
-<a href="http://localhost/geo/play.php?Zee_Anmol&q=1200"target="_blank">ZEE ANMOL<a> | 
-
-<h2> ENG NEWS</h2>
-<a href="http://localhost/geo/play.php?CNBC_Tv18_Prime_HD&q=1200"target="_blank">CNBC TV18 PRIME<a> | 
-<a href="http://localhost/geo/play.php?CNN&q=1200"target="_blank">CNN<a> | 
-<a href="http://localhost/geo/play.php?Times_Now_HD&q=1200"target="_blank">TIMES NOW<a> | 
-<a href="http://localhost/geo/play.php?BBC_World_News&q=1200"target="_blank">BBC WORLD NEWS<a> | 
-
-
-<h2> HINDI NEWS </h2>
-<a href="http://localhost/geo/play.php?Zee_Business&q=1200"target="_blank">ZEE BUSINESS<a> | 
-<a href="http://localhost/geo/play.php?Zee_News&q=1200"target="_blank">ZEE NEWS<a> | 
-
-
-
-
-
-
-<h2> INFORMATION </h2>
-<a href="http://localhost/geo/play.php?NDTV_Good_Times&q=1200"target="_blank">NDTV GOODS TIMES<a> |
-<a href="http://localhost/geo/play.php?Sony_BBC_Earth_HD&q=1200"target="_blank">SONY BBC EARTH H<a> | 
-<a href="http://localhost/geo/play.php?Travel_XP_HD&q=1200"target="_blank">TRAVEL XP<a> | 
-<a href="http://localhost/geo/play.php?TLC_HD_World&q=1200"target="_blank">TLC HD WORLD<a> | 
-<a href="http://localhost/geo/play.php?Animal_Planet_HD&q=1200"target="_blank">ANIMAL PLANET <a> | 
-<a href="http://localhost/geo/play.php?History_HD&q=1200"target="_blank">HISTORY TV HD<a> | 
-<a href="http://localhost/geo/play.php?Discovery_HD_World&q=1200"target="_blank">DISCOVERY HD WORLD<a> | 
-<a href="http://localhost/geo/play.php?FYI_TV18&q=1200"target="_blank">FYI TV18<a> | 
-<a href="http://localhost/geo/play.php?TLC_Hindi&q=1200"target="_blank">TLC HINDI<a> | 
-
-
-
-
-<h2> MUSIC </h2>
-<a href="http://localhost/geo/play.php?c=9XM&q=1200"target="_blank">9XM HQ<a> | 
-
-
-
-
-
-
-
-<a href="http://localhost/geo/play.php?ZZ&q=1200"target="_blank">ZZ<a> | 
-<a href="http://localhost/geo/play.php?ZZ&q=1200"target="_blank">ZZ<a> | 
-<a href="http://localhost/geo/play.php?ZZ&q=1200"target="_blank">ZZ<a> | 
-<a href="http://localhost/geo/play.php?ZZ&q=1200"target="_blank">ZZ<a> | 
-
-http://146.88.24.24/play.php?9XM&q=1200
-
-https://techandy4u.blogspot.com/2017/08/india-and-asian-iptv-playlist.html
-
-</html>
